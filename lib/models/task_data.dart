301df1f83b6a3a7dba55ea4d40ea9616115796c1
danaFlorescu <mariadana977@gmail.com>
2023-02-22 11:19:44 +0200

    refactoring task_data to have private tasks

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: "Buy milk"),
     Task(name: "Buy bread"),
     Task(name: "Pet Enzo"),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String newTaskTitle) {
-    tasks.add(
+    _tasks.add(
       Task(name: newTaskTitle),
     );
     notifyListeners();
